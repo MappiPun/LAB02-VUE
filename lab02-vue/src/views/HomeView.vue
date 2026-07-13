aacaeb7bca864f729372f346ab2b189df543c0b6
Mappi <iamappipun@gmail.com>
2026-07-13 14:23:50 +0700

    Checkpoint 8.4

--- lab02-vue/src/views/HomeView.vue
+++ lab02-vue/src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import EventCard from '@/components/EventCard.vue'
+import eventsData from '../events.json' 
 </script>
 
 <template>
   <div class="home">
-    <EventCard />
+
+    <EventCard 
+      v-for="event in eventsData" 
+      :key="event.id" 
+      :event="event" 
+    />
   </div>
 </template>
