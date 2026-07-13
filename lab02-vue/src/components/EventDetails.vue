b7e026d77d3e916479d69b05bb973e39cc60a475
Mappi <iamappipun@gmail.com>
2026-07-13 14:30:49 +0700

    Checkpoint 8.7

---
+++ lab02-vue/src/components/EventDetails.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { Event } from '@/types'
+
+defineProps<{
+  event: Event
+}>()
+</script>
+
+<template>
+  <div class="event-details">
+    <div>{{ event.category }}</div>
+    <div>{{ event.organizer }}</div>
+  </div>
+</template>
+
+<style scoped>
+.event-details {
+  text-align: right;
+  /* หากต้องการ font size ที่ชัดเจนสามารถกำหนดเพิ่มได้ที่นี่ */
+}
+</style>
--- lab02-vue/src/views/HomeView.vue
+++ lab02-vue/src/views/HomeView.vue
@@ -1,15 +1,23 @@
 <script setup lang="ts">
 import EventCard from '@/components/EventCard.vue'
-import eventsData from '../events.json' 
+import EventDetails from '@/components/EventDetails.vue'
+import eventsData from '../events.json'
 </script>
 
 <template>
-  <div class="home">
+  <div class="events">
 
-    <EventCard 
-      v-for="event in eventsData" 
-      :key="event.id" 
-      :event="event" 
-    />
+    <div v-for="event in eventsData" :key="event.id">
+      <EventCard :event="event" />
+      <EventDetails :event="event" />
+    </div>
   </div>
 </template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
